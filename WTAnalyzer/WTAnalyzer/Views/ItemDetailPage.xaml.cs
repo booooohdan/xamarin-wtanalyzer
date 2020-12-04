cbc20e4a6045d375a307cccd69f0e42dbb19e38d
Bogdan <bo7145907@gmail.com>
2020-12-05 00:40:20 +0200

    Добавьте файлы проекта.

---
+++ WTAnalyzer/WTAnalyzer/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using WTAnalyzer.ViewModels;
+using Xamarin.Forms;
+
+namespace WTAnalyzer.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
