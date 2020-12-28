ae019d55bb5cba965e089a7c847978477bf46a94
Bogdan <bo7145907@gmail.com>
2020-12-29 01:07:44 +0200

    Color converter is working

---
+++ WTAnalyzer/WTAnalyzer/Helpers/StringToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WTAnalyzer.Helpers
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color color = Color.White;
+            string colorStr = value.ToString();
+            switch (colorStr)
+            {
+                case "Premium":
+                    color = Color.FromHex("#ffecb4");
+                    break;
+                case "Promotional":
+                    color = Color.FromHex("#ffecb4");
+                    break;
+            }
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Color.White;
+        }
+    }
+}
--- WTAnalyzer/WTAnalyzer/Models/ListViewItem.cs
+++ WTAnalyzer/WTAnalyzer/Models/ListViewItem.cs
@@ -6,15 +6,19 @@ namespace WTAnalyzer.Models
 {
     public class ListViewItem
     {
-        public ListViewItem(string flag, string name, object value, object br)
+        public ListViewItem(string flag, string role, string vtype, string name, object value, object br)
         {
             Flag = flag;
+            Role = role;
+            Vtype = vtype;
             Name = name;
             Value = value;
             Br = br;
         }
 
         public string Flag { get; set; }
+        public string Role { get; set; }
+        public string Vtype { get; set; }
         public string Name { get; set; }
         public object Value { get; set; }
         public object Br { get; set; }
