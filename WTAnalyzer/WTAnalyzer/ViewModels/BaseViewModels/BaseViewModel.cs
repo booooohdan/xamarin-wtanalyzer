4e823c8c9f9d47d0ade40a7090223d1453563f80
Bogdan <bo7145907@gmail.com>
2021-02-10 18:52:12 +0200

    Set pass data between pages

--- WTAnalyzer/WTAnalyzer/ViewModels/BaseViewModels/BaseViewModel.cs
+++ WTAnalyzer/WTAnalyzer/ViewModels/BaseViewModels/BaseViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using WTAnalyzer.Models;
+using Xamarin.Forms;
 
 namespace WTAnalyzer.ViewModels.BaseViewModels
 {
@@ -15,6 +18,26 @@ namespace WTAnalyzer.ViewModels.BaseViewModels
                 busy = value;
                 OnPropertyChanged();
             }
+        }
+
+        private ListViewItem selectedVehicle;
+        public ListViewItem SelectedVehicle
+        {
+            get => selectedVehicle;
+            set
+            {
+                if (value != null)
+                {
+                    selectedVehicle = value;
+                    OnPropertyChanged();
+                    NavigateToOnePointBr();
+                }
+            }
+        }
+
+        private async Task NavigateToOnePointBr()
+        {
+            await Shell.Current.GoToAsync($"//onepointbr?name={SelectedVehicle.Name}");
         }
 
         public void OnPropertyChanged([CallerMemberName] string propertyName = "")
---
+++ WTAnalyzer/WTAnalyzer/ViewModels/OnePointBrViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WTAnalyzer.ViewModels.BaseViewModels;
+using Xamarin.Forms;
+
+namespace WTAnalyzer.ViewModels
+{
+    [QueryProperty("Name", "name")]
+    public class OnePointBrViewModel : BaseViewModel
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = Uri.UnescapeDataString(value);
+                OnPropertyChanged();
+            }
+        }
+
+        public OnePointBrViewModel()
+        {
+
+        }
+    }
+}
